baae9e231746aa15f2ee1b3a3121f0121cd4bb35
uniquebabytw <uniquebabytw@gmail.com>
2025-07-14 20:50:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,32 @@ inputName.addEventListener("input", function () {
       element.textContent = typedValue;   
       element.classList.remove("placeholder-text");
     }
+ const fontsToCheck = [
+  'LXGWMarkerGothic-Regular',
+  'PopGothicCjkTc-Regular',
+  '王汉宗行楷体繁',
+  'edukai-5.0',
+  'NotoSansTC-Regular',
+  'wt021',
+  '王漢宗中圓體繁',
+  'KosefontP-JP',
+  'Mamelon',
+  '宅在家自動筆20231015',
+  'wp010-08',
+  'BpmfGenSenRounded-M'
+];
+
+// 檢查字型是否成功載入
+fontsToCheck.forEach(fontName => {
+  document.fonts.load(`16px '${fontName}'`).then(fonts => {
+    const isLoaded = fonts.length > 0;
+    if (!isLoaded) {
+      // 找出對應使用該字型的元素
+      document.querySelectorAll(`.main-text`).forEach(el => {
+        if (getComputedStyle(el).fontFamily.includes(fontName)) {
+          el.textContent = '無法支援';
+        }
+      });
+    }
   });
 });
-
